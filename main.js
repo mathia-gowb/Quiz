7c28caefcd52c0798ffb625111a6f6132405b7a7
mathia-gowb <Tobvins@gmail.com>
2021-09-29 21:13:29 +0200

    added the complete results screen

--- main.js
+++ main.js
@@ -43,19 +43,19 @@ let optionSelected=false;
 let currAnswer="";
 /* FOR TRACKING THE QUESTION WE ARE IN ---> THIS INDEX WILL ALSO BE USED FOR THE BACK OPTION AND FOWARD OPTION */
 let question_index=0;
+let finalScore=0;
 /* will need to check if there is something at the current question  
 if there is someting it means that the question has already been answered (take the answer from the answers history)
 else the question has not been answered */
 const answers_history=[];
-
 function loadQuiz(){
     controlUiButtons();
     const currQuestion=questions[question_index];
     /* refreshes the ui */
     html_elements.options_container.textContent="";
-    html_elements.question_header.textContent=currQuestion.question+" ?";
+    handleContentInsert('header',currQuestion.question,"?");
     currQuestion.possible_answers.forEach((answer,i)=>{
-        html_elements.options_container.insertAdjacentHTML("beforeend",html_elements.create_answers(i,answer,answer))
+        handleContentInsert('inner',html_elements.create_answers(i,answer,answer))
     }) ;
     optionSelected=false;
     const options = document.querySelectorAll('input[type=radio]').forEach(el=>el.addEventListener('click',function (){
@@ -65,6 +65,11 @@ function loadQuiz(){
 }
 function handleElement(element,property){
     html_elements[element].style.display=property;
+}
+function handleContentInsert(targetEl,content,question=""){
+    if(targetEl==="header") html_elements.question_header.textContent=content+" "+question;
+    if(targetEl==="inner") html_elements.options_container.insertAdjacentHTML("beforeend",content)
+    
 }
 function controlUiButtons(){
     if(question_index===questions.length-1){
@@ -92,15 +97,19 @@ html_elements.back_button.addEventListener('click',()=>{
 })
 html_elements.proceed_button.addEventListener('click',()=>{
     /* go to the previously answered question */
+    if(currAnswer===answers[question_index]){
+        finalScore++;
+    }
     if(optionSelected){
         question_index++;
         loadQuiz();
-        
     }
-
+    /* check if the answer is equal to the expected answer */
 })
 html_elements.complete_button.addEventListener('click',()=>{
     if(optionSelected){
-
+        html_elements.options_container.textContent="";
+        handleContentInsert('header',"Your Final Score For This Quiz Is");
+        handleContentInsert('inner',`<br><br><h2 style="text-align:center">${finalScore}/${questions.length}</h2>`)
     }
 })
